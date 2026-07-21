9d1b6bf0bb344f2c47b7064e680eda6a7f299f8c
Emelie Hansson <emeliehanssonn@hotmail.se>
2026-07-21 20:54:28 +0200

    added search for suggestions and question page

--- src/app/dashboard/handler/questions/page.tsx
+++ src/app/dashboard/handler/questions/page.tsx
@@ -5,6 +5,7 @@ import { api } from "~/trpc/react";
 import { HiQuestionMarkCircle } from "react-icons/hi";
 import QuestionCard from "~/app/_components/cards/questionCard";
 import { FaRegQuestionCircle } from "react-icons/fa";
+import { FiSearch } from "react-icons/fi";
 
 const PAGE_SIZE = 5;
 
@@ -15,6 +16,7 @@ export default function QuestionsPage() {
     null,
   );
   const [newMessage, setNewMessage] = useState("");
+  const [search, setSearch] = useState("");
   const utils = api.useUtils();
   const { data: questions = [], isLoading } =
     api.question.listQuestions.useQuery({
@@ -59,6 +61,10 @@ export default function QuestionsPage() {
     );
   }
 
+  const filteredQuestions = questions.filter((question) =>
+    question.question.toLowerCase().includes(search.toLowerCase()),
+  );
+
   return (
     <main className="main-page-layout">
       <div className="container">
@@ -67,21 +73,40 @@ export default function QuestionsPage() {
           <h1 className="page-header">Frågor & Svar</h1>
         </div>
 
+        <div className="relative mt-6">
+          <FiSearch
+            className="absolute top-1/2 left-4 -translate-y-1/2 text-white/40"
+            size={18}
+          />
+
+          <input
+            type="text"
+            placeholder="Sök bland frågor..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="w-full rounded-xl border border-white/10 bg-white/5 py-3 pr-4 pl-11 text-white transition-all outline-none placeholder:text-white/40 focus:border-purple-500 focus:bg-white/10"
+          />
+        </div>
         <div className="mt-4 space-y-3">
-          {questions.map((question) => {
+          {filteredQuestions.map((question) => {
             return (
               <div className="card">
                 <button
+                  className="w-full"
                   onClick={() => {
                     toggleQuestions(question.id);
                   }}
                 >
-                  <div className="flex items-center justify-between">
-                    <h2 className="font-medium">{question.question}</h2>
-                  </div>
-                  <div className="mt-2 text-xs text-white/40">
-                    {question.createdBy?.name} ·{" "}
-                    {new Date(question.createdAt).toLocaleDateString()}
+                  <div className="flex flex-col gap-2">
+                    <div className="flex-1 self-start">
+                      <h2 className="font-medium">{question.question}</h2>
+                    </div>
+                    <div className="self-start text-xs text-white/40">
+                      {question.createdBy?.name
+                        ? `${question.createdBy.name} · `
+                        : "Anonym · "}
+                      {new Date(question.createdAt).toLocaleDateString()}
+                    </div>
                   </div>
                 </button>
                 {selectedQuestionId === question.id && (
--- src/app/dashboard/handler/suggestions/page.tsx
+++ src/app/dashboard/handler/suggestions/page.tsx
@@ -8,6 +8,7 @@ import SkeletonSuggestionCard from "~/app/_components/skeletonComponents/cards/s
 import { AnimatePresence, motion } from "framer-motion";
 import { GoDotFill, GoTrophy } from "react-icons/go";
 import { RiArrowUpDoubleFill } from "react-icons/ri";
+import { FiSearch } from "react-icons/fi";
 
 type FilterType = "latest" | "popular" | "status";
 
@@ -23,7 +24,7 @@ export default function SuggestionsHandlerPage() {
   const utils = api.useUtils();
 
   const [filter, setFilter] = useState<FilterType>("latest");
-
+  const [search, setSearch] = useState("");
   const { data: suggestions = [], isLoading } =
     api.suggestionBox.listSuggestions.useQuery();
 
@@ -54,25 +55,33 @@ export default function SuggestionsHandlerPage() {
     </motion.div>
   );
 
-  const sortedSuggestions = suggestions?.sort((a, b) => {
-    if (filter === "latest") {
-      return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
-    }
+  const visibleSuggestions = useMemo(() => {
+    const filtered = suggestions.filter((suggestion) =>
+      suggestion.content.toLowerCase().includes(search.toLowerCase()),
+    );
 
-    if (filter === "popular") {
-      return b.voteCount - a.voteCount;
-    }
+    return filtered.sort((a, b) => {
+      if (filter === "latest") {
+        return (
+          new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+        );
+      }
 
-    const order = {
-      SENT: 0,
-      UNDER_REVIEW: 1,
-      APPROVED: 2,
-      IMPLEMENTED: 3,
-      REJECTED: 4,
-    };
+      if (filter === "popular") {
+        return b.voteCount - a.voteCount;
+      }
 
-    return order[a.status] - order[b.status];
-  });
+      const order = {
+        SENT: 0,
+        UNDER_REVIEW: 1,
+        APPROVED: 2,
+        IMPLEMENTED: 3,
+        REJECTED: 4,
+      };
+
+      return order[a.status] - order[b.status];
+    });
+  }, [suggestions, search, filter]);
 
   if (isLoading) {
     return (
@@ -92,42 +101,58 @@ export default function SuggestionsHandlerPage() {
           <h1 className="page-header">Förslag från butikerna</h1>
         </div>
         {/* FILTER */}
-        <div className="flex gap-3">
-          <button
-            onClick={() => setFilter("latest")}
-            className={`flex items-center gap-2 rounded-lg px-3 py-2 text-sm ${
-              filter === "latest"
-                ? "bg-white/20"
-                : "bg-white/5 hover:bg-white/10"
-            }`}
-          >
-            <FaRegClock />
-            Senaste
-          </button>
+        <div className="flex justify-between">
+          <div className="flex items-end gap-3">
+            <button
+              onClick={() => setFilter("latest")}
+              className={`flex items-center gap-2 rounded-lg px-3 py-2 text-sm ${
+                filter === "latest"
+                  ? "bg-white/20"
+                  : "bg-white/5 hover:bg-white/10"
+              }`}
+            >
+              <FaRegClock />
+              Senaste
+            </button>
 
-          <button
-            onClick={() => setFilter("popular")}
-            className={`flex items-center gap-2 rounded-lg px-3 py-2 text-sm ${
-              filter === "popular"
-                ? "bg-white/20"
-                : "bg-white/5 hover:bg-white/10"
-            }`}
-          >
-            <GoTrophy />
-            Mest röster
-          </button>
+            <button
+              onClick={() => setFilter("popular")}
+              className={`flex items-center gap-2 rounded-lg px-3 py-2 text-sm ${
+                filter === "popular"
+                  ? "bg-white/20"
+                  : "bg-white/5 hover:bg-white/10"
+              }`}
+            >
+              <GoTrophy />
+              Mest röster
+            </button>
 
-          <button
-            onClick={() => setFilter("status")}
-            className={`flex items-center gap-2 rounded-lg px-3 py-2 text-sm ${
-              filter === "status"
-                ? "bg-white/20"
-                : "bg-white/5 hover:bg-white/10"
-            }`}
-          >
-            <GoDotFill />
-            Status
-          </button>
+            <button
+              onClick={() => setFilter("status")}
+              className={`flex items-center gap-2 rounded-lg px-3 py-2 text-sm ${
+                filter === "status"
+                  ? "bg-white/20"
+                  : "bg-white/5 hover:bg-white/10"
+              }`}
+            >
+              <GoDotFill />
+              Status
+            </button>
+          </div>
+          <div className="relative mt-4 w-100">
+            <FiSearch
+              className="absolute top-1/2 left-4 -translate-y-1/2 text-white/40"
+              size={18}
+            />
+
+            <input
+              type="text"
+              placeholder="Sök bland förslag..."
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              className="w-full rounded-xl border border-white/10 bg-white/5 py-3 pr-4 pl-11 text-white transition-all outline-none placeholder:text-white/40 focus:border-purple-500 focus:bg-white/10"
+            />
+          </div>
         </div>
         {/* LIST */}
         {isLoading && (
@@ -138,11 +163,11 @@ export default function SuggestionsHandlerPage() {
           </div>
         )}
         <div className="space-y-4">
-          {!isLoading && sortedSuggestions?.length === 0 && (
+          {!isLoading && visibleSuggestions?.length === 0 && (
             <p className="text-sm text-white/60">Inga idéer än</p>
           )}
 
-          {sortedSuggestions?.map((suggestion) => (
+          {visibleSuggestions?.map((suggestion) => (
             <div
               key={suggestion.id}
               className="flex gap-6 rounded-xl bg-white/5 p-4 shadow-lg/15 hover:bg-white/10"
