4367e71c1318d992f8be6c38a39d4403b77dd0e7
Emelie Hansson <emeliehanssonn@hotmail.se>
2026-08-02 13:46:55 +0200

    removed old migrations

--- prisma/migrations/20260721201205_add_question_embedding/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "News" ADD COLUMN "embedding" TEXT;
-
--- AlterTable
-ALTER TABLE "Question" ADD COLUMN "embedding" TEXT;
-
--- AlterTable
-ALTER TABLE "Suggestion" ADD COLUMN "embedding" TEXT;
--- prisma/migrations/20260729162700/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Resource" ADD COLUMN "embedding" TEXT;
-
--- AlterTable
-ALTER TABLE "Ticket" ADD COLUMN "embedding" TEXT;
